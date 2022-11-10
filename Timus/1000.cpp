c50ae6471d28e39a19338e2a012a97f30e2a5098
Syeda Nowshin Ibnat <47534922+Nowshin1077@users.noreply.github.com>
2022-11-10 20:37:26 +0600

    Add files via upload

---
+++ Timus/1000.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    
+    cin >> a >> b;
+
+    // sum of two numbers in stored in variable sumOfTwoNumbers(c)
+    c = a + b;
+
+    // Prints sum 
+    cout << c <<"\n";     
+
+    return 0;
+}
---
+++ Timus/1001. Reverse Root.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long num;
+    
+    cin >> num;
+
+    //double y;
+   // y = int (x);    // functional notation
+    double result = sqrt(num);
+
+    cout << result <<"\n";     
+
+    return 0;
+}
